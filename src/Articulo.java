f1f603e5408f7c1dfe6fb9c7592a59a58e7be930
Niitram88 <martincosasdeclase@gmail.com>
2024-02-07 10:44:27 +0100

    THIRD COMMIT

--- src/Articulo.java
+++ src/Articulo.java
@@ -1,12 +1,72 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Articulo {
     private int codi;
     private String descripcio;
     private double preucompra;
     private double preuventa;
     private int stock;
-
+    Scanner teclat = new Scanner(System.in);
     public Articulo(int codi, String descripcio, double preucompra, double preuventa, int stock){
+        this.codi = codi;
+        this.descripcio = descripcio;
+        this.preucompra = preucompra;
+        this.preuventa = preuventa;
+        this.stock = stock;
+    }
+    public Articulo(){
 
     }
 
+    public void setCodi(int codi) {
+        this.codi = codi;
+    }
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+    public void setPreucompra(double preucompra) {
+        this.preucompra = preucompra;
+    }
+    public void setPreuventa(double preuventa) {
+        this.preuventa = preuventa;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public int getCodi() {
+        return codi;
+    }
+    public String getDescripcio() {
+        return descripcio;
+    }
+    public double getPreucompra() {
+        return preucompra;
+    }
+    public double getPreuventa() {
+        return preuventa;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void llistat(){
+        System.out.println(this.descripcio + " en codi " + this.codi );
+    }
+    public static Articulo añade(ArrayList <Articulo> tempo){
+        Scanner teclat = new Scanner(System.in);
+        Articulo a = new Articulo();
+        System.out.println("Introduce los valores (el codigo se asignara solo):");
+        a.codi = tempo.size()+1;
+        System.out.println(tempo.size()+1);
+        System.out.print("Nombre:");
+        a.descripcio = teclat.nextLine();
+        System.out.print("Preu de compra:");
+        a.preucompra = teclat.nextDouble();
+        System.out.print("Preu de venta:");
+        a.preuventa = teclat.nextDouble();
+        System.out.print("Quantitat en almacen:");
+        a.stock = teclat.nextInt();
+        teclat.nextLine();
+        return a;
+    }
 }
--- src/Gestisimag.java
+++ src/Gestisimag.java
@@ -1,5 +1,89 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gestisimag {
+    public static void main(String[] args) {
+        int menu = 0, supp = 0;
+        Scanner teclat = new Scanner(System.in);
+        ArrayList <Articulo> articulos = new ArrayList<Articulo>();
+        articulos.add( new Articulo(1,"Portatil gaming",400,700,56));
+        articulos.add( new Articulo(2,"Ordenador gaming",700,1200,36));
 
+        System.out.println("Bienvenido al menu de interaccion de GESTISIMAG");
+        do{
+            System.out.println("1. Llistat\n2. Alta\n3. Baixa\n4. Modificació\n5. Entrada de mercaderia\n6. Eixida de mercaderia\n7. Eixir");
+            menu = teclat.nextInt();
+            switch (menu){
+                case 1:
+                    for(Articulo i: articulos){
+                        i.llistat();
+                    }
+                    break;
+                case 2:
+                    articulos.add(Articulo.añade(articulos));
+                    break;
+                case 3:
+                    System.out.println("Que articulo quieres eliminar de la lista:");
+                    for(Articulo i: articulos){
+                        i.llistat();
+                    }
+                    supp = teclat.nextInt();
+                    boolean compro = false;
+                    for(int cont = 0; cont < articulos.size(); cont++){
+                        if (supp -1 == cont){
+                            System.out.println("Objeto encontrado, eliminando");
+                            articulos.remove(supp -1);
+                            compro = true;
+                        }
+                    }
+                    if(compro == false){
+                        System.err.println("Articulo no encontrado, no se eliminara nada");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Quin producte vols modificar:");
+                    for(Articulo i: articulos){
+                        i.llistat();
+                    }
+                    supp = teclat.nextInt();
+                    System.out.println("Que campo deseas modificar");
+                    System.out.println("1.- Nombre\n2.- Precio de Compra\n3.- Precio de Venta");
+                    int menu2 = teclat.nextInt();
+                    teclat.nextLine();
+                    switch(menu2){
+                        case 1:
+                            System.out.println("Introduzca el nuevo nombre");
+                            String cambio = teclat.nextLine();
+                            articulos.get(supp-1).setDescripcio(cambio);
+                            break;
+                        case 2:
+                            System.out.println("Introduzca el nuevo precio de compra");
+                            double cambioprecio = teclat.nextDouble();
+                            articulos.get(supp-1).setPreucompra(cambioprecio);
+                            break;
+                        case 3:
+                            System.out.println("Introduzca el nuevo precio de venta");
+                            cambioprecio = teclat.nextDouble();
+                            articulos.get(supp).setPreuventa(cambioprecio);
+                            break;
+                        default:
+                            System.out.println("Opcion no encontrada");
+                            break;
+                    }
+                    for(Articulo i: articulos){
+                        i.llistat();
+                    }
+                    break;
+                case 5:
+                    System.out.println("A quin producte vols afexir stock:");
+                    for(Articulo i: articulos){
+                        i.llistat();
+                    }
+                    supp = teclat.nextInt();
+
+            }
+
+        }while (menu != 7);
+    }
 }
 
