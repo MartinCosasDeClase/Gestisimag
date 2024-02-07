c4bee7cf52d5fed13481f5dcc7e003cf7b76d00d
Niitram88 <martincosasdeclase@gmail.com>
2024-02-07 09:23:33 +0100

    SECOND COMMIT

---
+++ src/Articulo.java
@@ -0,0 +1,12 @@
+public class Articulo {
+    private int codi;
+    private String descripcio;
+    private double preucompra;
+    private double preuventa;
+    private int stock;
+
+    public Articulo(int codi, String descripcio, double preucompra, double preuventa, int stock){
+
+    }
+
+}
